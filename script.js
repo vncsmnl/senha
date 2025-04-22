01c2ef3ef7908e832c1d900bb37bacc5fdea3d8b
Vinícius Manoel <59481808+vncsmnl@users.noreply.github.com>
2025-04-22 00:35:45 -0300

    :sparkles: feat: add more checkboxs

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const password = document.querySelector("#password");
 
 const containerPassword = document.querySelector("#container-password");
 
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const special = "!@#$%&*()_+-=[]{}|;:,.<>?/";
+
 let newPass = "";
 
 sizePassword.innerHTML = sliderElement.value;
@@ -15,18 +19,32 @@ slider.oninput = function () {
     sizePassword.innerHTML = this.value;
 }
 
-function genPass() {
+function getSelectedCharset() {
+    const includeLowercase = document.querySelector("#includeLowercase").checked;
+    const includeUppercase = document.querySelector("#includeUppercase").checked;
+    const includeNumbers = document.querySelector("#includeNumbers").checked;
+    const includeSpecial = document.querySelector("#includeSpecial").checked;
 
+    let selectedCharset = "";
+    if (includeLowercase) selectedCharset += lowercase;
+    if (includeUppercase) selectedCharset += uppercase;
+    if (includeNumbers) selectedCharset += numbers;
+    if (includeSpecial) selectedCharset += special;
+
+    return selectedCharset || lowercase; // Default to lowercase if nothing selected
+}
+
+function genPass() {
+    const charset = getSelectedCharset();
     let pass = "";
 
-    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
-        pass += charset.charAt(Math.floor(Math.random() * n));
+    for (let i = 0; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * charset.length));
     }
 
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
     newPass = pass;
-
 }
 
 function copyPass() {
